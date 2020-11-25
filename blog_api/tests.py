0d33cf5666c7228e38ffaa5f2d2065546cfb1cf3
Abhishek Banerjee <abhishek.banerjee18@outlook.com>
2020-11-26 02:14:37 +0530

    basic testing done

--- blog_api/tests.py
+++ blog_api/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from blog.models import Post, Category
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class PostTests(APITestCase):
+    # Can we make a post from our API
+
+    def test_view_posts(self):
+        url = reverse('blog_api:listcreate')
+        # client = browser
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def create_post(self):
+        self.test_category = Category.objects.create(name='django')
+
+        self.testuser1 = User.objects.create_user(username='test_user1', password='password')
+
+        data = {"title": "new", "author": 1, "excerpt": "new", "content": "new"}
+        url = reverse('blog_api:listcreate')
+        response = sef.client.post(url, data, format='json')
+        self.asserEqual(response.status_code, status.HTTP_201_CREATED)
+
+
