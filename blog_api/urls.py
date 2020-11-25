3ce00fc634cb598665ee8e640b15c95a73a95631
Abhishek Banerjee <abhishek.banerjee18@outlook.com>
2020-11-25 22:47:04 +0530

    Serializer done

--- blog_api/urls.py
+++ blog_api/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-# from .views import PostList, PostDetail
+from .views import PostList, PostDetail
 
 
 app_name = 'blog_api'
 
 urlpatterns = [
-    # path('<int:pk>/', PotDetail.as_view(), name='detailcreate'),
-    # path('', PostList.as_view(), name='listcreate'),
+    path('<int:pk>/', PostDetail.as_view(), name='detailcreate'),
+    path('', PostList.as_view(), name='listcreate'),
     
     ]
